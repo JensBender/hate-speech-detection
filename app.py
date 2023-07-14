29b11712e0761eda814734b665916aa9c3284693
Jens Bender <jens.bender.001@gmail.com>
2023-07-14 11:29:36 +0200

    in app.py, add api route

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ Created on Mon Jul  3 08:39:20 2023
 @author: Jens Bender
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -20,6 +20,7 @@ load_dotenv()
 
 # Create the Flask web application
 app = Flask(__name__)
+
 # Set a secret key (stored in .env) as a security measure (e.g. protecting against CSRF attacks) 
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
@@ -61,6 +62,27 @@ def home():
                                prediction=prediction, 
                                prediction_prob=prediction_prob)
     return render_template("index.html", form=form)
+
+
+# API route
+@app.route("/api")
+def prediction_by_api():
+    # Get the input text from the api query parameter
+    input_text = request.args.get("comment")
+    # Convert input text to a list
+    input_data = [input_text]
+    # Make prediction using the TensorFlow model
+    prediction_prob = model.predict(input_data)[0][0]
+    # Convert prediction probability to prediction in text form
+    if prediction_prob >= 0.5:
+        prediction = "Hate Speech"
+    else:
+        prediction = "No Hate Speech"
+        # Invert the prediction probability
+        prediction_prob = 1 - prediction_prob
+    # Return json with the prediction and prediction probability
+    return jsonify({"prediction": prediction,
+                    "probability": float(prediction_prob)})
 
 
 # Start the Flask web application
